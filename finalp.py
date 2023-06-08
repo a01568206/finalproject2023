77b61cf8a591ca81308193fc289010a90267feef
a01568206 <135772973+a01568206@users.noreply.github.com>
2023-06-08 11:51:08 -0600

    Update finalp.py

--- finalp.py
+++ finalp.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yfinance as yf
+import streamlit as st
 
 # para para descargar los datos y limpiarlos
 def obtener_precios_activos(symbol, start_date, end_date):
@@ -8,20 +9,27 @@ def obtener_precios_activos(symbol, start_date, end_date):
     return df
 
 # inputs
-symbol = input("Ingrese el símbolo del activo: ")
-start_date = input("Ingrese la fecha de inicio (YYYY-MM-DD): ")
-end_date = input("Ingrese la fecha de fin (YYYY-MM-DD): ")
+symbol = st.text_input("Ingrese el símbolo del activo: ")
+start_date = st.text_input("Ingrese la fecha de inicio (YYYY-MM-DD): ")
+end_date = st.text_input("Ingrese la fecha de fin (YYYY-MM-DD): ")
 
-# Obtener los precios y limpiar la base de datos
-precios = obtener_precios_activos(symbol, start_date, end_date)
+if st.button("Buscar"):
+    #df = yf.download(symbol, start=start_date, end=end_date)
+    #precios = df[['Adj Close']].dropna()
+    precios = yf.Ticker(symbol).history(start=start_date, end=end_date)
 
-# Mostrar los primeros 5 y los últimos 5 datos
-primeros_5 = precios.head(5)
-ultimos_5 = precios.tail(5)
+    # - accurate time-series count:
+    #precios = ticker.history(start=start_date, end=end_date)
 
-# Mostrar los resultados
-st.write("\nPrimeros 5 datos:")
-st.write(primeros_5)
+    st.write(precios)
+    
+    #Mostrar los primeros 5 y los últimos 5 datos
+    primeros_5 = precios.head(5)
+    ultimos_5 = precios.tail(5)
+    
+    # Mostrar los resultados
+    st.write("\nPrimeros 5 datos:")
+    st.write(primeros_5)
 
-st.write("\nÚltimos 5 datos:")
-st.write(ultimos_5)
+    st.write("\nÚltimos 5 datos:")
+    st.write(ultimos_5)
